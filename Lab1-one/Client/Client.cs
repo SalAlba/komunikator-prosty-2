f4bf28c0228b9a86228836dd56151faec6bc289b
SalAlba <saljan93@live.com>
2017-04-06 21:49:16 +0200

    first commit

---
+++ Lab1-one/Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Client
+    {
+        // Fields ...
+        private const string IP_ADRESS = "127.0.0.1";
+        private const int PORT = 2222;
+        private static Socket socket;
+
+        // main ...
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("1) Hi I'm client :)");
+            Console.WriteLine("2) Conect to server");
+            Console.WriteLine("--------------------------------------");
+            connect();
+
+            send("FIRST MSG FROM CLIENT CHECKING :))");
+
+            while (true) {
+                string msg = Console.ReadLine();
+                send(msg);
+                if (msg.Contains("quit")) break;
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Press Enter to finish !!");
+
+
+            Console.ReadLine();
+        }
+
+        // Methods ...
+        private static void connect() {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(IP_ADRESS), PORT);
+            socket.Connect(ipEndPoint);
+        }
+
+        private static void send(string msg){
+            Byte[] byteData = Encoding.ASCII.GetBytes(msg.ToCharArray());
+            socket.Send(byteData, byteData.Length, 0);
+        }
+
+    }
+}
---
+++ Lab1-one/Server/Server.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Server
+    {
+        // Fields ...
+        private const string IP_ADRESS = "127.0.0.1";
+        private const int PORT = 2222;
+        private const int DATAGRAM_SIZE = 10;
+        private static int num = 0;
+        private static TcpListener tcpListener;
+        private static Socket socket;
+
+        // main ...
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("1) Hi I'm server :)");
+            Console.WriteLine("2) Sever start...");
+            Console.WriteLine("--------------------------------------");
+
+            startServer();
+            reciveData();
+         
+            Console.ReadLine();
+            stopServer();
+        }
+
+        
+        ///  methods ....
+        
+        // START
+        private static void startServer() {
+            tcpListener = new TcpListener(IPAddress.Parse(IP_ADRESS), PORT);
+            tcpListener.Start(); // START 
+            socket = tcpListener.AcceptSocket();
+        }
+
+        // STOP 
+        private static void stopServer() { tcpListener.Stop();  }
+
+
+        private static void reciveData() {
+            int flag = 1;
+
+            while (flag == 1)
+            {
+
+                // recive data
+                Byte[] reciveData = new Byte[DATAGRAM_SIZE];
+                int error = socket.Receive(reciveData, reciveData.Length, 0);
+                string msg = null;
+                msg = System.Text.Encoding.ASCII.GetString(reciveData);
+
+                if (msg.Length > 0)
+                {
+                    Console.WriteLine("{0} Client msg ) {1} ;", num++, msg);
+
+                    if (msg.Contains("quit"))
+                    {
+                        flag = 0;
+                        Console.WriteLine("--------------------------------------");
+                        Console.WriteLine("Press Enter to finish !!");
+                    }
+
+                }
+            }
+        }
+
+
+
+    }
+}
